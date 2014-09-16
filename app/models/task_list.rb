65debb3ed11d0c8f59078b8ad0961572c9c9aeaa
Helen Hood <helen.c.hood@gmail.com>
2014-09-16 15:53:56 -0400

    Add associations with TaskList and User

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,5 +1,7 @@
 class TaskList < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
+  has_many :user_task_lists, dependent: :destroy
+  has_many :users, through: :user_task_lists
 
   validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_many :tasks
+  has_many :user_task_lists, dependent: :destroy
+  has_many :task_lists, through: :user_task_lists
 
   validates :email,
     presence: true,
