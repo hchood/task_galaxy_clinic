7dd4b1ea2a32dbde9a323eb86a7668210bf4f3ac
Helen Hood <helen.c.hood@gmail.com>
2014-09-16 15:57:05 -0400

    Add assigned_users association to TaskList

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -2,6 +2,7 @@ class TaskList < ActiveRecord::Base
   has_many :tasks, dependent: :destroy
   has_many :user_task_lists, dependent: :destroy
   has_many :users, through: :user_task_lists
+  has_many :assigned_users, through: :tasks, class_name: 'User'
 
   validates :name, presence: true
 end
