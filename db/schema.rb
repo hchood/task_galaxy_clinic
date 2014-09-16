874d2c5b575418b5e4b6f0c3674a5132c2896fe3
Helen Hood <helen.c.hood@gmail.com>
2014-09-16 15:42:10 -0400

    Add task_list_id to tasks table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916193758) do
+ActiveRecord::Schema.define(version: 20140916194131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,11 +24,12 @@ ActiveRecord::Schema.define(version: 20140916193758) do
   end
 
   create_table "tasks", force: true do |t|
-    t.string   "name",        null: false
+    t.string   "name",         null: false
     t.text     "description"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "task_list_id"
   end
 
   create_table "users", force: true do |t|
